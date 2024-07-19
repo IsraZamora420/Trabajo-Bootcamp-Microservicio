a3fa1528dc7f9b1b79c918dc2fbbce87cadf6cf9
Jimmy Ochoa <jimmyochoaholguin@gmail.com>
2024-07-18 20:12:09 -0500

    Actualizacion de archivos

---
+++ Trabajo Bootcamp Microservicio/Models/Modulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trabajo_Bootcamp_Microservicio.Models;
+
+public partial class Modulo
+{
+    public int ModuloId { get; set; }
+
+    public string? ModuloDescripcion { get; set; }
+
+    public short? Estado { get; set; }
+
+    public DateTime? FechaHoraReg { get; set; }
+
+    public DateTime? FechaHoraAct { get; set; }
+
+    public int? UsuIdReg { get; set; }
+
+    public int? UsuIdAct { get; set; }
+
+    public virtual ICollection<Opcion> Opcions { get; set; } = new List<Opcion>();
+}
